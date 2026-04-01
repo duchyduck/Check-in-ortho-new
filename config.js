3c18da41b12324cd4f548a80b1a78662bbf16a58
DUCHY <nichakan.ke.budc@cpird.in.th>
2026-04-01 15:10:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,12 +13,12 @@
 const CONFIG = {
     // 🌐 URL ของ Google Apps Script (ตัวใหม่ที่รวมโค้ดแล้ว)
     // สามารถใช้ลิงก์เดียวกันได้เลย เพราะระบบ Backend แยกส่วนให้แล้ว
-    WEB_APP_API: "https://script.google.com/macros/s/AKfycbzUwMzks27DNUDGYQRwdnGeUWb7fvjBW1knwBp3sDQtdq1i3jQ60zmWkGFsZPh-ZDk_SA/exec",
+    WEB_APP_API: "https://script.google.com/macros/s/AKfycby5i7WH3nqPtKGtsJpTamCsLqLBUmux7drKvZt9vc_rfEBSz1O_em085yyd1c0luEfq/exec",
 
     // 📱 LIFF IDs
-    LIFF_ID_CHECKIN: "2008088008-48JzVeeQ",
-    LIFF_ID_HISTORY: "2008088008-jWGqwvvn",
-    LIFF_ID_REGISTER: "2008088008-RGaXeww4",
+    LIFF_ID_CHECKIN: "2009669862-AHx6dvg2",
+    LIFF_ID_HISTORY: "2009669862-hNlbJFm8",
+    
 
     // 📍 การตั้งค่าพิกัดสถานที่ (ละติจูด, ลองจิจูด)
     TARGET_LATITUDE: 16.807157,
